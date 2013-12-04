482151c75625dfdec47d23a30dafae7fe463034e
Yoann Ramos <yramos@epsi.fr>
2013-12-04 16:41:11 +0100

    Initial

---
+++ todo.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Yoann on 04/12/13.
+ */
+function TodoCtrl($scope){
+    $scope.totalTodos = 4;
+
+    $scope.todos = [
+        {text:'Learn AngularJS', done:false}
+        {text:'Build an app', done:false}
+    ];
+}
