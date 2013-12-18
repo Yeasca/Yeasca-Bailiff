9a4f0762d01171707c06073d36fe6d386340792a
Dowmed <info.aroche@gmail.com>
2013-12-18 12:03:39 +0100

    Ajout 2 menus applications / modif main.css et main.js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,22 @@ function openMoreFilter() {
         $('#filtreplus').slideToggle();
 
 }
+
+function openCustomers() {
+    $('#fichierclient').slideToggle();
+
+}
+
+function openUsers() {
+    $('#utilisateurs').slideToggle();
+
+}
+
+function openAccount() {
+    $('#compte').slideToggle();
+
+}
+
+function openUtilisateurs(id) {
+    $('#compte'+id).slideToggle();
+}
