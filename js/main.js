a3736b5bee17d80e223e5e11dcfa322a8a73e333
Yoann Ramos <yramos@epsi.fr>
2013-12-18 15:42:27 +0100

    Ajout liens morts

--- js/main.js
+++ js/main.js
@@ -26,7 +26,12 @@ function openUtilisateurs(id) {
 var d = new Date();
 var mois = d.getMonth()+1;
 var jour = d.getDate();
+var heures = d.getHours()
+var minutes = d.getMinutes()
+var heureReconstruite = (heures<10 ? '0' : '') + heures
+    + ':' + (minutes<10 ? '0' : '') + minutes;
 var dateReconstruite = (jour<10 ? '0' : '') + jour
     + '/' + (mois<10 ? '0' : '') + mois + '/'
     +  d.getFullYear();
+$('#constat-heure').html(heureReconstruite);
 $('#constat-date').html(dateReconstruite);
