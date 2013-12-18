97efeced02bc3a7af491860e12af48648f4ddcad
Yoann Ramos <yramos@epsi.fr>
2013-12-18 15:19:42 +0100

    Get Date

--- js/main.js
+++ js/main.js
@@ -21,3 +21,12 @@ function openAccount() {
 function openUtilisateurs(id) {
     $('#compte'+id).slideToggle();
 }
+
+
+var d = new Date();
+var mois = d.getMonth()+1;
+var jour = d.getDate();
+var dateReconstruite = (jour<10 ? '0' : '') + jour
+    + '/' + (mois<10 ? '0' : '') + mois + '/'
+    +  d.getFullYear();
+$('#constat-date').html(dateReconstruite);
